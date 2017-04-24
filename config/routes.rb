2e9db4b52a048f3a76af04bb6675e3b9362cd0ff
Sam <samuel.burns.25@gmail.com>
2017-04-24 19:28:55 -0400

    fb keys added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :posts
 
   root 'posts#index'
+
+  get '/auth/:provider/callback' => 'sessions#create'
+  delete '/signout' => 'sessions#destroy', :as => :signout
+  post '/signin' => 'sessions#new', :as => :signin
 end
