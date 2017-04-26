aec7fb47c4f9cfd37200ce541b26c6e167adbc2b
Sam <samuel.burns.25@gmail.com>
2017-04-25 22:36:26 -0400

    simple style adds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
+Post.destroy_all
 Post.create(dog_name: "Skip", image_url: "https://img0.etsystatic.com/125/0/10692466/il_340x270.981333968_ily8.jpg", description: "Captain Golden")
+Post.create(dog_name: "Champ", image_url: "https://media.xogrp.com/images/83126cd9-afdb-40ec-9b2f-7f304996914c~rt_auto", description: "Champ looking royal!")
+Post.create(dog_name: "Wrinkles", image_url: "http://media.spokesman.com/photos/2016/09/30/mart_Halloween_Media_Preview.JPG.jpg", description: "You're under aruffest")
+Post.create(dog_name: "Sir Barksley", image_url: "https://s-media-cache-ak0.pinimg.com/originals/1f/0a/04/1f0a04d79e63f0d54a036657bb1a054b.jpg", description: "King of Pride Rock")
+Post.create(dog_name: "Baxter", image_url: "http://humor.gunaxin.com/wp-content/uploads/gallery/pet-costumes/d60420a5682767f7a47df90c9782e98f.jpg", description: "Extra bacon please")
+Post.create(dog_name: "King", image_url: "https://s-media-cache-ak0.pinimg.com/736x/49/f6/2e/49f62e55b3725d413f3cc1c4e2730b63.jpg", description: "There's a new hero in town")
