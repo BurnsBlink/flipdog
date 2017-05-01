934fdac9d8bbbeec744da27e4f9706f4cfb04f07
Sam <samuel.burns.25@gmail.com>
2017-05-01 10:56:08 -0400

    react working

---
+++ react/src/containers/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
+import Home from '../components/Home';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+
+      <Router history={browserHistory}>
+        <Route path="/" component={Home}>
+        </Route>
+
+      </Router>
+    )
+  }
+}
+
+export default App;
