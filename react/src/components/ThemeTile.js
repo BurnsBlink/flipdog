25f8d1a282b2303ccbf21a777217352586c26124
Sam <samuel.burns.25@gmail.com>
2017-05-11 16:41:15 -0400

    menu fix

--- react/src/components/ThemeTile.js
+++ react/src/components/ThemeTile.js
@@ -5,13 +5,12 @@ const ThemeTile = props => {
   return (
     <div className="row">
       <div className="large-4 columns">
-      <div className="winner">
-        <h4><span className="finalist">Human:</span> <span className="human-finalist">{props.winnerHuman}</span></h4>
-        <h5><span className="finalist">Dog:</span> <span className="dog-finalist">{props.winnerDogName} ({props.winnerVotes})</span></h5>
-        <p className="description-finalist">{props.winnerDescription}</p>
-        <img className="image-finalist" src={props.winnerImage}/>
-      </div>
-
+        <div className="winner">
+          <h4><span className="finalist">Human:</span> <span className="human-finalist">{props.winnerHuman}</span></h4>
+          <h5><span className="finalist">Dog:</span> <span className="dog-finalist">{props.winnerDogName} ({props.winnerVotes})</span></h5>
+          <p className="description-finalist">{props.winnerDescription}</p>
+          <img className="image-finalist" src={props.winnerImage}/>
+        </div>
       </div>
 
       <div className="large-4 columns">
@@ -21,17 +20,17 @@ const ThemeTile = props => {
           <p className="description-finalist">{props.secondDescription}</p>
           <img className="image-finalist" src={props.secondImage}/>
         </div>
-
       </div>
 
       <div className="large-4 columns">
-      <div className="third">
-        <h4><span className="finalist">Human:</span> <span className="human-finalist">{props.thirdHuman}</span></h4>
-        <h5><span className="finalist">Dog:</span> <span className="dog-finalist">{props.thirdDogName} ({props.thirdVotes})</span></h5>
-        <p className="description-finalist">{props.thirdDescription}</p>
-        <img className="image-finalist" src={props.thirdImage}/>
+        <div className="third">
+          <h4><span className="finalist">Human:</span> <span className="human-finalist">{props.thirdHuman}</span></h4>
+          <h5><span className="finalist">Dog:</span> <span className="dog-finalist">{props.thirdDogName} ({props.thirdVotes})</span></h5>
+          <p className="description-finalist">{props.thirdDescription}</p>
+          <img className="image-finalist" src={props.thirdImage}/>
+        </div>
       </div>
-      </div>
+    <button className="back" onClick={browserHistory.goBack}>Back</button><br/><br/>
     </div>
   );
 }
