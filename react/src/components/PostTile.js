a26b1e9f6b30a291841d0589cb8c9340f86c9900
Sam <samuel.burns.25@gmail.com>
2017-05-14 21:53:35 -0400

    heroku fix

--- react/src/components/PostTile.js
+++ react/src/components/PostTile.js
@@ -11,7 +11,6 @@ class PostTile extends Component {
   }
 
   handleVoteCount(){
-    event.preventDefault()
     let postId = this.props.id
     fetch(`/api/v1/posts/${postId}`, {
       method: 'PUT',
